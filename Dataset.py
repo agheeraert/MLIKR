0e24db3d5740085d84cf9f52142b6e3eec31e0fb
Aria GHEERAERT <aria.gheeraert@gmail.com>
2019-02-01 17:20:00 +0100

    few changes

--- Dataset.py
+++ Dataset.py
@@ -4,18 +4,16 @@ from os.path import join as jn
 import pickle
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import numpy as np
 
 BASE_FOLDER = '/home/agheerae/Python/MLIKR'
 
 def dump(obj, path):
     return pickle.dump(obj, open(path, 'wb'))
 
-# filenames = [jn(BASE_FOLDER, 'data/dcd', elt) for elt in listdir('data/dcd')]
-filenames = [jn(BASE_FOLDER, 'data/dcd/prot_apo_sim1_s10.dcd')]
-
+filenames = [jn(BASE_FOLDER, 'data/dcd', elt) for elt in listdir('data/dcd')]
 #Featurizing the angles
-psi_features = featurize_all(filenames, DihedralFeaturizer, jn(BASE_FOLDER, 'data/prmtop/prot.prmtop'), chunk=100)
-
+psi_features = featurize_all(filenames, DihedralFeaturizer, jn(BASE_FOLDER, 'data/prmtop/prot.prmtop'), chunk=1000) 
 # Retrieving values (bound=1, unbound=0)
 Y_tot = []
 for elt in psi_features[2]:
@@ -23,13 +21,15 @@ for elt in psi_features[2]:
         Y_tot.append(1)    
     elif elt[-14] == 'o':
         Y_tot.append(0)
-
+print('Featurizing distances')
 #Featurizing the distances
-d_features = featurize_all(filenames, BinaryContactFeaturizer, jn(BASE_FOLDER, 'data/prmtop/prot.prmtop'), chunk=100)
-X_tot = np.concatenate(psi_features[0], ca_features[0], axis=1)
+d_features = featurize_all(filenames, BinaryContactFeaturizer, jn(BASE_FOLDER, 'data/prmtop/prot.prmtop'), chunk=125)
+print(d_features)
+X_tot = np.concatenate((psi_features[0], d_features[0]), axis=1)
+print(X_tot)
 
 #Splitting in random train and test sets
-X_train, X_test, Y_train, Y_test = train_test_split([X_tot, Y_tot])
+X_train, X_test, Y_train, Y_test = train_test_split(X_tot, Y_tot, test_size=0.33, random_state=42)
 
 #Dumping
 dump(X_train, 'X_train.p')
--- pre_training.py
+++ pre_training.py
@@ -1,4 +1,18 @@
 from sklearn import tree
 import pickle as pkl
 
-tree = tree.DecisionTreeClassifier()
+def load(path):
+    return pkl.load(open(path, 'rb'))
+
+X_test = load('X_test.p')
+X_train = load('X_train.p')
+Y_test = load('Y_test.p')
+Y_train = load('Y_train.p')
+
+clf = tree.DecisionTreeClassifier(random_state=0)
+clf = clf.fit(X_train, Y_train)
+
+Y_pred = clf.predict(X_test)
+
+acc_decision_tree = round(clf.score(X_train, Y_train) * 100, 2)
+print(round(acc_decision_tree,2,), "%")
